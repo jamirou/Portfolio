f02f96eb27cd3404c2db2c7b9f3c9879bb60f9db
Jamiro <jamirocristian15@gmail.com>
2023-10-23 13:11:04 -0300

    w

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import styled from "styled-components"
-import Contact from "./components/Contact"
-import Hero from "./components/Hero"
-import Who from "./components/Who"
-import Works from "./components/Works"
+import styled from "styled-components";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
 
 const Container = styled.div`
-height: 100vh;
-scroll-snap-type: y mandatory;
-scroll-behavior: smooth;
-overflow-y: auto;
-scrollbar-width: none;
-color: white;
-background: url("./img/bg.jpeg");
-&::-webkit-scrollbar{
-  display:none
-}
-`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
 
 function App() {
-
   return (
     <Container>
       <Hero />
@@ -26,7 +25,7 @@ function App() {
       <Works />
       <Contact />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
