37600f683093a6d61fe62dd4712f1e4d9995770b
Jamiro <jamirocristian15@gmail.com>
2023-10-23 00:18:55 -0300

    ,

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
+import styled from "styled-components"
 import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import Who from "./components/Who"
 import Works from "./components/Works"
 
+const Container = styled.div`
+height: 100vh;
+background-color: rebeccapurple;
+`
+
 function App() {
 
   return (
-    <div>
+    <Container>
       <Hero/>
       <Who/>
-      <Works/>
+      <Works/> 
       <Contact/>
-    </div>
+    </Container>
   )
 }
 
