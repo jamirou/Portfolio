d1172a56f87d670ee58c594ad6405c5bb32d9dc1
Jamiro <jamirocristian15@gmail.com>
2023-10-24 11:23:18 -0300

    'm'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Hello</h1>
+      <h1 className="text-4xl font-bold font-signature">Hello</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      signature: ['Great Vibes'],
+    }
   },
   plugins: [],
 }
