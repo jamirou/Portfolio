2a3150c4595d4bda42325e36f30318a0b61a830a
Jamiro <jamirocristian15@gmail.com>
2023-10-25 04:09:20 -0300

    adding imports in experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import reactImage from "../assets/react.png";
+import nextjs from "../assets/nextjs.png";
+import graphql from "../assets/graphql.png";
+import github from "../assets/github.png";
+import tailwind from "../assets/tailwind.png";
+
+
 const Experience = () => {
   return (
     <div>Experience</div>
