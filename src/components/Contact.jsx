e50692ca51457e74f8dfdc21e7003e5ac94bf756
Jamiro <jamirocristian15@gmail.com>
2023-10-25 04:38:36 -0300

    adding action to form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/08a617cf-40fa-4569-a553-5b0e61a23677"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
