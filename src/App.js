c6a06804e2000058019475bc8a4fa65d803a866b
Jamiro <jamirocristian15@gmail.com>
2023-10-25 04:06:41 -0300

    porftolio section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Navbar/>
       <Home/>
       <About/>
+      <Portfolio/>
+      <Experience/>
       
       <SocialLinks/>
     </div>
