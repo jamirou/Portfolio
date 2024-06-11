e05f2a2f9582a0e9a156be218f20fce78601b5a8
jaguilera <devjamiro@gmail.com>
2024-06-11 00:55:37 -0400

    add fun to copy mail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
+  const [copyMessage, setCopyMessage] = useState("");
+
+  const handleEmailClick = () => {
+    const email = "devjamiro@gmail.com";
+    navigator.clipboard.writeText(email).then(() => {
+      setCopyMessage("¡Copied!");
+      setTimeout(() => {
+        setCopyMessage("");
+      }, 2000); 
+    });
+  };
+
   return (
     <footer className="w-full bg-black">
       <div className="flex flex-col items-center py-4">
         <div className="text-center">
           <p className="text-lg text-white">Get in Touch</p>
-          <p className="mt-2 text-gray-400">devjamiro@gmail.com</p>
+          <p 
+            className="mt-2 text-gray-400 cursor-pointer" 
+            onClick={handleEmailClick}
+          >
+            devjamiro@gmail.com
+          </p>
+          {copyMessage && <p className="text-green-500 mt-2">{copyMessage}</p>}
         </div>
         <div className="flex mt-4 space-x-4">
           <a href="https://www.linkedin.com/in/jamiro-manriquez-832aaa204/" target="_blank" rel="noopener noreferrer">
