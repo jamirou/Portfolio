8abe8870da9ca9278e169600f346d1e193f08dbf
jaguilera <devjamiro@gmail.com>
2024-06-12 21:34:25 -0400

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
 
   const links = [
     {
-      id: 1, 
+      id: 1,
       link: "home",
     },
     {
@@ -31,12 +31,14 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-2xl font-code ml-2 hover:text-gray-400 transition duration-300 ease-in-out transform hover:scale-110">
-          Jamiro.dev
-        </h1>
+        <Link to="home" smooth duration={500}>
+          <h1 className="text-2xl font-code ml-2 hover:text-gray-400 transition duration-300 ease-in-out transform hover:scale-110 cursor-pointer">
+            Jamiro.dev
+          </h1>
+        </Link>
       </div>
 
-      <ul className="hidden md:flex"> 
+      <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
             key={id}
