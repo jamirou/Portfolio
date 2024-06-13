e258d27db485330f0ad48d3e2fdd6636f77597fb
jaguilera <devjamiro@gmail.com>
2024-06-12 21:16:54 -0400

    update dependences

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Typewriter } from "react-simple-typewriter";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faAndroid, faApple, faReact } from "@fortawesome/free-brands-svg-icons"; // Importa los iconos nuevos
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faAndroid, faApple, faReact } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   const [copyMessage, setCopyMessage] = useState("");
