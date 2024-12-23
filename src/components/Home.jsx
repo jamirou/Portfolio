f9e4d79fd40304dd367961504169e0ec9193a1d6
Jamiro <devjamiro@gmail.com>
2024-12-23 10:25:36 -0300

    feat(home): update home description and translation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Fade } from 'react-awesome-reveal';
 const Home = () => {
     return (
         <div
-            name="home"
+            name="inicio"
             className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
         >
             <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4">
