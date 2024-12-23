43f69eb712adcd3415d65f883813d481b6f8064b
Jamiro <devjamiro@gmail.com>
2024-12-23 10:24:40 -0300

    feat(porfolio): update portfolio description

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -79,7 +79,7 @@ const Portfolio = () => {
 
   return (
     <div
-      name="portfolio"
+      name="proyectos"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
@@ -111,7 +111,7 @@ const Portfolio = () => {
                     onMouseEnter={() => setHoveredId(project.id)}
                     onMouseLeave={() => setHoveredId(null)}
                   >
-                    {hoveredId === project.id ? "Github" : "Code"}
+                    {hoveredId === project.id ? "Github" : "Repositorio"}
                   </a>
                 </div>
               </div>
